71a6b5671760cb9260d135d005e82f7e50532b3e
CarlosAGuardado <carlosguardado98@yahoo.com>
2023-05-01 10:35:11 -0400

    favorite stuff

---
+++ functions/simple_functions.js
@@ -0,0 +1,16 @@
+function favoriteThings() {
+    console.log("cars")
+    console.log("soccer")
+    console.log("family")
+}
+favoriteThings() ;
+
+function whyImHere() {
+    console.log('I joined this course because I am looking for a better future for my family')
+}
+whyImHere() ;
+
+function favoritePlace() {
+    console.log('my favorite place is my hometown in Honduras')
+}
+favoritePlace() ;
